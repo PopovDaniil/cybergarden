326d0c68a3beac01a3f791393ed6a4947d9daad1
Daniil <pde-rnd@yandex.ru>
2021-05-29 21:13:58 +0300

    Fixed .global selector, added margin to .content, implemented toggling of forms

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,24 @@
 //слайдер
 document.addEventListener('DOMContentLoaded', function () {
-    const slider = new ChiefSlider('.slider', {
-      loop: true,
-      autoplay: true,
-      interval: 3000
-    }) ?? null;
+  const slider = new ChiefSlider('.slider', {
+    loop: true,
+    autoplay: true,
+    interval: 3000
+  }) ?? null;
 
-    /* выпадающее меню входа */
-    let btn = document.querySelector(".header-conteiner__login");
-    let menu = document.querySelector(".conteiner");
-    btn.addEventListener('click', function () {
-        menu.classList.toggle("show");
-    })
+  /* выпадающее меню входа */
+  let btn = document.querySelector(".header-conteiner__login");
+  let menu = document.querySelector(".conteiner");
+  btn.addEventListener('click', function () {
+    menu.classList.toggle("show");
+  })
 
-  let membersForm = document.querySelector('#members')
-  let organizersForm = document.querySelector('#organizers')
+});
 
-
-  });
+function setForm(index) {
+  document.querySelector('#members').classList.toggle('hide')
+  document.querySelector('#organizers').classList.toggle('hide')
+}
 
 
 
