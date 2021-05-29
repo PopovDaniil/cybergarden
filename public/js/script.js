2747f0644a9d71ed4158dac5cad05b5c32b125d7
Daniil <pde-rnd@yandex.ru>
2021-05-29 21:31:30 +0300

    Account menu now uses onclick

--- public/js/script.js
+++ public/js/script.js
@@ -5,15 +5,11 @@ document.addEventListener('DOMContentLoaded', function () {
     autoplay: true,
     interval: 3000
   }) ?? null;
-
-  /* выпадающее меню входа */
-  let btn = document.querySelector(".header-conteiner__login");
-  let menu = document.querySelector(".conteiner");
-  btn.addEventListener('click', function () {
-    menu.classList.toggle("show");
-  })
-
 });
+
+function toggleDropdown() {
+  document.querySelector(".conteiner").classList.toggle("show");
+}
 
 function setForm(index) {
   document.querySelector('#members').classList.toggle('hide')
