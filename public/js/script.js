40e9fe323c3ba75e8bd405dfe724aa3e4fea3d19
Vlasov <maksim>
2021-05-29 10:17:51 +0300

    Added images and styles

---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+//слайдер
+document.addEventListener('DOMContentLoaded', function () {
+    const slider = new ChiefSlider('.slider', {
+      loop: true,
+      autoplay: true,
+      interval: 3000
+    });
+
+    /* выпадающее меню входа */
+    let btn = document.querySelector(".header-conteiner__login");
+    let menu = document.querySelector(".conteiner");
+    btn.addEventListener('click', function () {
+        menu.classList.toggle("show");
+    })
+
+  });
+
+
+
+
