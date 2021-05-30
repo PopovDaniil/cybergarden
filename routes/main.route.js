ef6c6a525df86d95887c0a6c0df28b97bf8f703f
Daniil <pde-rnd@yandex.ru>
2021-05-30 12:49:23 +0300

    Fixed authorization

--- routes/main.route.js
+++ routes/main.route.js
@@ -1,5 +1,6 @@
 import plugin from "fastify-plugin"
 import eventModel from "../models/event.model.js"
+import userModel from "../models/user.model.js"
 
 /**
  * 
@@ -9,8 +10,9 @@ async function index(fastify) {
     fastify.get('/', async (request, reply) => {
         const events = await eventModel.find().lean()
         const userId = await request.session.get('_id')
-        console.log(request.session);
-        reply.view('index', { events, index: true, userId })
+        const user = await userModel.findById(userId)
+        console.log(user);
+        reply.view('index', { events, index: true, user })
     })
 }
 
